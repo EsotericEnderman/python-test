192e7af00e14bd4aa1bce933a444767370c26e2f
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-10-02 14:44:00 +0100

    Count the vowels

--- src/count_vowels.py
+++ src/count_vowels.py
@@ -1,7 +1,11 @@
 english_vowels = [ "a", "e", "i", "o", "u" ]
 
 word = "random"
+vowel_count = 0
 
 for letter in word:
     if letter in english_vowels:
         print(letter)
+        vowel_count += 1
+
+print("The word '" + word + "' has " + str(vowel_count) + " vowels.")
