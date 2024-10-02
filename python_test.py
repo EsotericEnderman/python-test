729b41dd4f5a76341a95d8eefe54a7028c911562
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-10-02 13:59:13 +0100

    Add baby case

--- python_test.py
+++ python_test.py
@@ -7,9 +7,16 @@ if age > 24:
 else:
     old = False
 
+if age < 5:
+    baby = True
+else:
+    baby = False
+
 print("Hello, " + name + ". You are " + str(age) + " years old.")
 
 if old:
     print("You have one foot in the grave!")
+elif baby:
+    print("You are a baby! 👶")
 else:
     print("You are still young!")
