82c8ad5bb06b6eabb235864cae7334f8ebbe3a5d
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-10-02 15:36:33 +0100

    Avoid infinite loop

--- src/count_unique_vowels.py
+++ src/count_unique_vowels.py
@@ -12,5 +12,6 @@ while letter_index < len(string_input):
     if letter in english_vowels and letter not in seen_vowels:
         seen_vowels.append(letter)
         unique_vowel_count += 1
+    letter_index += 1
 
 print("The string + '" + string_input + "' contains " + str(unique_vowel_count) + " unique vowels!")
