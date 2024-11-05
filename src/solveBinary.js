6c2981567679c5d97ed2d2717d98ac6c3ad4bbed
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-05 14:42:49 +0000

    Add decimal converter

--- src/solveBinary.js
+++ src/solveBinary.js
@@ -5,6 +5,7 @@ const binary = readFileSync("src/binary.txt").toString().split("\n").map(Number)
 for (const bin of binary) {
     console.log("Octal: " + toOctal(bin));
     console.log("Hex: " + toHex(bin));
+    console.log("Decimal: " + toDecimal(bin));
 }
 
 function toOctal(bin) {
@@ -13,4 +14,8 @@ function toOctal(bin) {
 
 function toHex(bin) {
     return parseInt(bin, 2).toString(16);
+}
+
+function toDecimal(bin) {
+    return parseInt(bin, 2).toString(10);
 }
