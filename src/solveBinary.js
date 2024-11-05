65ffbd0527ad7baa3def0ab5a0c40bc58d2704aa
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-05 14:44:26 +0000

    Revert "Add decimal converter"
    This reverts commit 6c2981567679c5d97ed2d2717d98ac6c3ad4bbed.

--- src/solveBinary.js
+++ src/solveBinary.js
@@ -5,7 +5,6 @@ const binary = readFileSync("src/binary.txt").toString().split("\n").map(Number)
 for (const bin of binary) {
     console.log("Octal: " + toOctal(bin));
     console.log("Hex: " + toHex(bin));
-    console.log("Decimal: " + toDecimal(bin));
 }
 
 function toOctal(bin) {
@@ -14,8 +13,4 @@ function toOctal(bin) {
 
 function toHex(bin) {
     return parseFloat(bin, 2).toString(16);
-}
-
-function toDecimal(bin) {
-    return parseFloat(bin, 2).toString(10);
 }
